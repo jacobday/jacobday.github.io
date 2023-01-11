f1f0b639732e1101bbfff629f060f88328c3df69
Jacob Day <26262942+jacobday@users.noreply.github.com>
2023-01-10 21:43:53 -0500

    added projects section

--- js/main.js
+++ js/main.js
@@ -34,6 +34,42 @@ function initSignature() {
   });
 }
 
+function initProjects(dataJSON) {
+  dataJSON.forEach((element, index) => {
+    const cardTemplate = document
+      .querySelector("#projectCard")
+      .content.cloneNode(true);
+
+    // Image container
+    const cardImage = cardTemplate.querySelector("img");
+    const imageObject = element.featureImage;
+
+    for (attribute of Object.entries(imageObject)) {
+      cardImage[attribute[0]] = attribute[1];
+    }
+
+    cardTemplate.querySelector(".label .index").innerHTML = `0${index + 1}`;
+    cardTemplate.querySelector(".label .name").innerHTML = element.name;
+
+    // Meta container
+    cardTemplate.querySelector(".description").innerHTML = element.description;
+
+    const primaryLink = cardTemplate.querySelector("a.primary");
+    const secondaryLink = cardTemplate.querySelector("a.secondary");
+
+    if ("caseStudy" in element) {
+      primaryLink.href = element.caseStudy.url;
+    } else {
+      primaryLink.style.display = "none";
+      secondaryLink.style.paddingLeft = 0;
+    }
+
+    secondaryLink.href = element.url;
+
+    document.querySelector(".projects .container").appendChild(cardTemplate);
+  });
+}
+
 function initObservers() {
   // Signature observers
   let signatureObserver = new IntersectionObserver(
@@ -59,5 +95,36 @@ function initObservers() {
   });
 }
 
+// Load projects data
+function loadJSON(fileName) {
+  // make a JSON loading object
+  const xobj = new XMLHttpRequest();
+
+  // prepare to read JSON files
+  xobj.overrideMimeType("application/json");
+
+  // specify the request type
+  xobj.open("GET", fileName);
+
+  // what to do once you read the file
+  xobj.onreadystatechange = () => {
+    // check if the file is loaded correctly
+    if (xobj.readyState == 4 && xobj.status == 200) {
+      // ready to read the JSON and process it
+      // first get the file content
+      const responseText = xobj.responseText;
+
+      // this is a raw string, convert from string to JSON object
+      const dataJSON = JSON.parse(responseText);
+
+      initProjects(dataJSON);
+    }
+  };
+
+  xobj.send();
+}
+
 initSignature();
 initObservers();
+
+loadJSON("./assets/data/projects.json");
