77a90a8e8d0c2851c1aef7a79412e1f11b624f68
Jacob Day <26262942+jacobday@users.noreply.github.com>
2023-01-11 21:49:56 -0500

    mobile styling

--- js/main.js
+++ js/main.js
@@ -68,6 +68,8 @@ function initProjects(dataJSON) {
 
     document.querySelector(".projects .container").appendChild(cardTemplate);
   });
+
+  initGallery();
 }
 
 function initObservers() {
@@ -92,6 +94,85 @@ function initObservers() {
   const signatures = document.querySelectorAll(".signature");
   signatures.forEach((signature) => {
     signatureObserver.observe(signature);
+  });
+}
+
+let scrollBuffer = 0;
+
+// Set gallery container height
+function setStickyContainersSize() {
+  requestAnimationFrame(function () {
+    document.querySelectorAll(".gallery-sticky").forEach(function (container) {
+      const stickyElement = container.querySelector(
+        ".container > div:first-of-type"
+      );
+
+      const stickyElementWidth = stickyElement.scrollWidth;
+      const stickyElementHeight = stickyElement.offsetHeight;
+
+      const stickyContainerHeight =
+        stickyElementHeight > stickyElementWidth
+          ? stickyElementHeight +
+            container.querySelector(".container").scrollWidth
+          : container.querySelector(".container").scrollWidth +
+            container.querySelector(".card").offsetHeight -
+            container.querySelector(".card").scrollWidth +
+            scrollBuffer * 2;
+
+      container.setAttribute(
+        "style",
+        "height: " + stickyContainerHeight + "px"
+      );
+    });
+  });
+}
+
+function trackScroll() {
+  // Find all the container elements
+  const containers = document.querySelectorAll(".gallery-sticky");
+
+  // Create an IntersectionObserver to track the containers
+  const observer = new IntersectionObserver((entries) => {
+    // Find the first container that is fully in view
+    const containerInViewPort = entries.find((entry) => entry.isIntersecting);
+    if (!containerInViewPort) return;
+
+    // Get the gallery element and the current scroll position
+    const gallery = containerInViewPort.target.querySelector(".gallery");
+    const scrollTop = document.documentElement.scrollTop;
+    const containerTop = containerInViewPort.target.offsetTop;
+
+    // Scroll the gallery horizontally
+    const pxToScroll = scrollTop - containerTop;
+    gallery.scrollLeft = pxToScroll;
+  });
+
+  // Start observing the containers
+  for (const container of containers) {
+    observer.observe(container);
+  }
+}
+
+function initGallery() {
+  setStickyContainersSize();
+
+  let galleryObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && entry.intersectionRatio > 0.1) {
+          document.addEventListener("scroll", trackScroll);
+        } else if (entry.intersectionRatio <= 0.1) {
+          document.removeEventListener("scroll", trackScroll);
+        }
+      });
+    },
+    { threshold: [0, 0.5] }
+  );
+
+  // Observe galleries
+  const galleries = document.querySelectorAll(".gallery");
+  galleries.forEach((gallery) => {
+    galleryObserver.observe(gallery);
   });
 }
 
@@ -124,7 +205,9 @@ function loadJSON(fileName) {
   xobj.send();
 }
 
+loadJSON("./assets/data/projects.json");
+
 initSignature();
 initObservers();
 
-loadJSON("./assets/data/projects.json");
+window.addEventListener("resize", setStickyContainersSize);
