4f1a8b99dce54655bdbbd255850d3654a7f34bf8
Jacob Day <26262942+jacobday@users.noreply.github.com>
2023-08-11 16:23:29 -0500

    added greeting

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { ComputersCanvas } from "../canvas";
+
+import styles from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={styles.hero}>
+      {/* Greeting Container */}
+      <div className={styles.greetingContainer}>
+        {/* Left Border Container */}
+        <div className={styles.border}>
+          {/* Circle */}
+          <div className={styles.circle} />
 
-export default Hero
+          {/* Line */}
+          <div className={styles.line} />
+        </div>
+
+        {/* Greeting Text */}
+        <div>
+          <h1>
+            Hi, I'm <span>Jacob</span>
+          </h1>
+
+          <h2>I develop 3D visuals, user interfaces, and web applications</h2>
+        </div>
+      </div>
+
+      {/* Computer Model */}
+      <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
